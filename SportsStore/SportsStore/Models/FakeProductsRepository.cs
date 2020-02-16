14de5a825c87826f881f27096cbdfc0a2c338bd4
Bartosz Majda <bartosz.majda32@gmail.com>
2020-02-16 21:51:38 +0100

    Unit test for page links

---
+++ SportsStore/SportsStore/Models/FakeProductsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductsRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product{Name = "Soccer ball",Price=25},
+            new Product{Name = "Surfing board", Price=179},
+            new Product{Name = "Shoes", Price = 95}
+        }.AsQueryable<Product>();
+    }
+}
---
+++ SportsStore/SportsStore/Models/SeedData.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices
+                .GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product()
+                    {
+                        Name = "Kyak",
+                        Description = "Boat for 1 person",
+                        Category = "Water Sports",
+                        Price = 275
+                    },
+                    new Product()    
+                    {
+                        Name = "Life jacket",
+                        Description = "Secure",
+                        Category = "Water Sports",
+                        Price = 48.95m
+                    },
+                    new Product()
+                    {
+                        Name = "Ball",
+                        Description = "Accepted by FIFA",
+                        Category = "Soccer",
+                        Price = 19.50m
+                    },
+                    new Product()
+                    {
+                        Name = "Corner Flag",
+                        Description = "Very good looking at soccer arena",
+                        Category = "Soccer",
+                        Price = 34.9m
+                    },
+                    new Product()
+                    {
+                        Name = "Stadion",
+                        Description = "Folding arena for 35000 people",
+                        Category = "Soccer",
+                        Price = 79500
+                    },
+                    new Product()
+                    {
+                        Name = "Cap",
+                        Description = "Increase brain accuracy over 75%",
+                        Category = "Chess",
+                        Price = 16
+                    },
+                    new Product()
+                    {
+                        Name = "Unstable chair",
+                        Description = "Decrease enemy chance for win",
+                        Category = "Chess",
+                        Price = 75,
+                    },
+                    new Product()
+                    {
+                        Name = "Human chess-board",
+                        Description = "Family game",
+                        Category = "Chess",
+                        Price = 75
+                    },
+                    new Product()
+                    {
+                        Name = "Shakes king",
+                        Description = "Figure made of gold and diamonds",
+                        Category = "Chess",
+                        Price = 1200
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+
+}
