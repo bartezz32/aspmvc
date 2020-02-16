fe18130fbcf0cdde0deecc9243e2ed30d5d9a88f
Bartosz Majda <bartosz.majda32@gmail.com>
2020-02-16 21:49:02 +0100

    chapter completed

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kyak", Price = 275M},
+                new Product {Name = "Lifejack", Price = 49M},
+                new Product {Name = "Foot ball", Price = 14M},
+                new Product {Name = "Flag", Price = 25M}
+            };
+            return View(array);
+        }
+    }
+
+}
