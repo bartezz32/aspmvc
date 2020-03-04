88c0a86923d505ca74654f3c3b2db44e54a3ff36
Bartosz Majda <bartosz.majda32@gmail.com>
2020-03-04 22:27:48 +0100

    Unit tests added

---
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Extensions.Internal;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+
+}
