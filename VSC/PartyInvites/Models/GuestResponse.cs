e25467a09b8af4c365c8996a19f6de47ba369a52
Bartosz Majda <bartosz.majda32@gmail.com>
2020-03-22 20:06:40 +0100

    Data model configuration

---
+++ VSC/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models{
+
+    public class GuestResponse{
+        public int id {get; set;}
+        [Required(ErrorMessage = "First name and last name")]
+        public string Name {get; set;}
+
+        [Required(ErrorMessage = "Email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage="Valid email")]
+        public string Email {get; set;}
+
+        [Required(ErrorMessage = "Phone number")]
+        public string Phone {get; set;} 
+
+        [Required(ErrorMessag = "Will you attend?")]
+        public bool? WillAttend {get; set;}
+    }
+}
