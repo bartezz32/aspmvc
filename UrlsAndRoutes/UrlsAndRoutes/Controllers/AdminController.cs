945316a6f05e5333c050b50644d4c4508f263356
Bartosz Majda <bartosz.majda32@gmail.com>
2020-04-29 22:40:58 +0200

    Custom Contstraints

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using UrlsAndRoutes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(AdminController),
+                Action = nameof(Index)
+            });
+    }
+}
---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using UrlsAndRoutes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ViewResult Index() => View("Result",
+            new Result {
+                Controller = nameof(CustomerController),
+                Action = nameof(Index)
+            });
+
+        public ViewResult List(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(List)
+
+            };
+            r.Data["id"] = id ?? "<no-value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
+    }
+}
