b458e868c8be04d8dfefe9fcc0f3464945288fa4
Bartosz Majda <bartosz.majda32@gmail.com>
2020-08-04 22:13:32 +0200

    Redirecting

--- ControllersAndActions/ControllersAndActions.Tests/ActionTests.cs
+++ ControllersAndActions/ControllersAndActions.Tests/ActionTests.cs
@@ -8,15 +8,27 @@ namespace ControllersAndActions.Tests
 {
     public class ActionTests
     {
-        
+
+
         [Fact]
-        public void NotFoundActionMethod()
+        public void ModelObjectType()
         {
+
             ExampleController controller = new ExampleController();
-            StatusCodeResult result = controller.Index();
-            Assert.Equal(404, result.StatusCode);
+            ViewResult result = controller.Index();
+            Assert.IsType<string>(result.ViewData["Message"]);
+            Assert.Equal("Hello", result.ViewData["Message"]);
+            Assert.IsType<System.DateTime>(result.ViewData["Date"]);
 
         }
-        
-    }
+
+        [Fact]
+        public void Redirect()
+        {
+            ExampleController controller = new ExampleController();
+            RedirectResult result = controller.Redirect();
+            Assert.Equal("/Example/Index", result.Url);
+            Assert.True(result.Permanent);
+        }
+    } 
 }
--- ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -8,8 +8,15 @@ namespace ControllersAndActions.Controllers
 {
     public class ExampleController : Controller
     {
-        public ViewResult Index() => View(DateTime.Now);
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Hello";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
 
         public ViewResult Result() => View((object) "Hello world!");
+
+        public RedirectResult Redirect() => RedirectPermanent("/Example/Index");
     }
 }
