8e52272a434cbe4a43274afa960cc6cce670e1a6
Bartosz Majda <bartosz.majda32@gmail.com>
2020-08-07 19:28:16 +0200

    loosely copled components

---
+++ DependencyInjection/DependencyInjection.Tests/DITests.cs
@@ -0,0 +1,27 @@
+﻿using DependencyInjection.Controllers;
+using DependencyInjection.Infrastructure;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Tests
+{
+    public class DITests
+    {
+        [Fact]
+        public void ControllerTest()
+        {
+            var data = new[] { new Product { Name = "Test", Price = 100 } };
+            var mock = new Mock<IRepository>();
+
+            mock.SetupGet(m => m.Products).Returns(data);
+            TypeBroker.SetTestObject(mock.Object);
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.Index();
+            Assert.Equal(data, result.ViewData.Model);
+        }
+    }
+
+}
--- DependencyInjection/DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/DependencyInjection/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DependencyInjection.Models;
+using DependencyInjection.Infrastructure;
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View();
+        public IRepository Repository { get; } = TypeBroker.Repository;
+        public ViewResult Index() => View(Repository.Products);
 
     }
 }
