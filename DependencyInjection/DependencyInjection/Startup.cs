b5a161e988c606edbd42635b829453d9934dad84
Bartosz Majda <bartosz.majda32@gmail.com>
2020-08-07 21:03:50 +0200

    scoped life cycle

--- DependencyInjection/DependencyInjection/Startup.cs
+++ DependencyInjection/DependencyInjection/Startup.cs
@@ -13,11 +13,28 @@ namespace DependencyInjection
 {
     public class Startup
     {
+        private IHostingEnvironment env;
+
+        public Startup(IHostingEnvironment hostEnv) => env = hostEnv;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IRepository, MemoryRepository>();
+            services.AddTransient<IRepository>(provider =>
+            {
+                if (env.IsDevelopment())
+                {
+                    var x = provider.GetService<MemoryRepository>();
+                    return x;
+                }
+                else
+                {
+                    return new AlternateRepository();
+                }
+            });
+            services.AddScoped<IRepository, MemoryRepository>();
+            services.AddTransient< MemoryRepository>();
             services.AddTransient<IModelStorage, DictionaryStorage>();
             services.AddTransient<ProductTotalizer>();
             services.AddMvc();
