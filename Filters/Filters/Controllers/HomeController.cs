b4b4f424325b26a0557815bd21201b4ae0512dcf
Bartosz Majda <bartosz.majda32@gmail.com>
2020-08-08 15:47:06 +0200

    Using filters

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [RequireHttps]
+        public IActionResult Index() => View("Message", "This is Index() method from HomeController");
+            
+        [RequireHttps]
+        public IActionResult SecondAction() => View("Message", "This is SecondAction() method from HomeController");
+   
+    }
+}
