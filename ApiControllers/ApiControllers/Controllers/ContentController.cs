5241998b99f39c585b04baaf7bb7347641b7dd4f
Bartosz Majda <bartosz.majda32@gmail.com>
2020-08-15 22:00:19 +0200

    Content Formatting

---
+++ ApiControllers/ApiControllers/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiControllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller
+    {
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public Reservation GetObject() => new Reservation
+        {
+            ReservationId = 100,
+            ClientName = "John",
+            Location = "Hall"
+        };
+    }
+
+}
