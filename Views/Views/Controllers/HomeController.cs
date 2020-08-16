c79ff7793f9b82db434806d216e6a6dc520da966
Bartosz Majda <bartosz.majda32@gmail.com>
2020-08-16 21:53:41 +0200

    Template added

---
+++ Views/Views/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Hello world";
+            ViewBag.Time = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        public ViewResult List() => View();
+    }
+}
