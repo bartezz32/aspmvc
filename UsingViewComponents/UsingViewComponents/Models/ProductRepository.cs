eeb457ce7d94d3d212b619614bb688c750f0a833
Bartosz Majda <bartosz.majda32@gmail.com>
2020-09-13 22:05:18 +0200

    Project template

---
+++ UsingViewComponents/UsingViewComponents/Models/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsingViewComponents.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+        void AddProduct(Product newProduct);
+    }
+
+
+    public class MemoryProductRepository : IProductRepository
+    {
+        private List<Product> products = new List<Product>
+        {
+            new Product{Name = "Kayak", Price = 275M},
+            new Product{Name="Lifejacket", Price = 48.25M},
+            new Product{Name = "Foot ball",Price = 10.5M}
+        };
+
+        public IEnumerable<Product> Products => products;
+
+        public void AddProduct(Product newProduct)
+        {
+            products.Add(newProduct);
+        }
+
+    } 
+}
